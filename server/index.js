711f036810118d413ee2ee0f83227e085c4a33aa
Ghulam Rasool <sanglavi@hotmail.com>
2023-02-19 01:52:17 +0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const connectDB = require("./config/db");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const schema = require("./schema/schema");
+const app = express();
+connectDB();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+app.listen(port, () => {
+  console.log(`server started at: ${port}`);
+});
---
+++ server/schema/sampleData.js
@@ -0,0 +1,31 @@
+const projects = [
+  {
+    id: "1",
+    clientId: "1",
+    name: "Project 1",
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
+  },
+  {
+    id: "2",
+    clientId: "2",
+    name: "Project 2",
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
+  },
+];
+
+const clients = [
+  {
+    id: "1",
+    name: "Tony Stark",
+    email: "tony@gmail.com",
+    phone: "1234567890",
+  },
+  {
+    id: "2",
+    name: "Nelson",
+    email: "nelsongmail.com",
+    phone: "1234567890",
+  },
+];
+
+module.exports = { projects, clients };
